fd9704addf1ab39170703064dc4387b846d28e92
Jan Boon <janboon438@gmail.com>
2017-07-05 15:30:53 +0200

    Scoredice added

--- js/hethoogstewoord.js
+++ js/hethoogstewoord.js
@@ -1,4 +1,6 @@
+var scoredice = [[[1,'b'],[2,'y'],[3,'r'],[4,'g']],[[1,'g'],[2,'b'],[3,'r'],[4,'y']],[[1,'y'],[2,'g'],[3,'b'],[4,'r']],[[1,'r'],[2,'y'],[3,'g'],[4,'b']],[[1,'b'],[2,'r'],[3,'y'],[4,'g']],[[1,'y'],[2,'b'],[3,'g'],[4,'r']]];
+var letterdices = [];
 function initialization() {
-	console.log("Initialization");	
+	console.log("Initialization " + scoredice[2]);
 }
 
