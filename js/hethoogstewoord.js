0c52f75bb423b5c8b97c8a7861e0818a548ae506
Jan Boon <janboon438@gmail.com>
2017-07-08 10:14:27 +0200

    Box shadow class added

--- js/hethoogstewoord.js
+++ js/hethoogstewoord.js
@@ -23,40 +23,76 @@ function newthrow() {
 	$("#letter1").html(letterdice1[ldice][0][0]);
 	$("#letter1").css('color', 'white');
 	$("#letter1").css('background-color', backgroundcolor(letterdice1[ldice][0][1]));
+	$("#letter1").on('click', function() {
+		$("#letter1").addClass('box-shadow');
+	});
 	$("#letter2").html(letterdice1[ldice][1][0]);
 	$("#letter2").css('color', 'white');
 	$("#letter2").css('background-color', backgroundcolor(letterdice1[ldice][1][1]));
+	$("#letter2").on('click', function() {
+		$("#letter2").addClass('box-shadow');
+	});
 	$("#letter3").html(letterdice1[ldice][2][0]);
 	$("#letter3").css('color', 'white');
 	$("#letter3").css('background-color', backgroundcolor(letterdice1[ldice][2][1]));
+	$("#letter3").on('click', function() {
+		$("#letter3").addClass('box-shadow');
+	});
 	$("#letter4").html(letterdice1[ldice][3][0]);
 	$("#letter4").css('color', 'white');
 	$("#letter4").css('background-color', backgroundcolor(letterdice1[ldice][3][1]));
+	$("#letter4").on('click', function() {
+		$("#letter4").addClass('box-shadow');
+	});
 	ldice = Math.floor((Math.random() * 6));
 	$("#letter5").html(letterdice2[ldice][0][0]);
 	$("#letter5").css('color', 'white');
 	$("#letter5").css('background-color', backgroundcolor(letterdice2[ldice][0][1]));
+	$("#letter5").on('click', function() {
+		$("#letter5").addClass('box-shadow');
+	});
 	$("#letter6").html(letterdice2[ldice][1][0]);
 	$("#letter6").css('color', 'white');
 	$("#letter6").css('background-color', backgroundcolor(letterdice2[ldice][1][1]));
+	$("#letter6").on('click', function() {
+		$("#letter6").addClass('box-shadow');
+	});
 	$("#letter7").html(letterdice2[ldice][2][0]);
 	$("#letter7").css('color', 'white');
 	$("#letter7").css('background-color', backgroundcolor(letterdice2[ldice][2][1]));
+	$("#letter7").on('click', function() {
+		$("#letter7").addClass('box-shadow');
+	});
 	$("#letter8").html(letterdice2[ldice][3][0]);
 	$("#letter8").css('color', 'white');
 	$("#letter8").css('background-color', backgroundcolor(letterdice2[ldice][3][1]));
+	$("#letter8").on('click', function() {
+		$("#letter8").addClass('box-shadow');
+	});
 	ldice = Math.floor((Math.random() * 6));
 	$("#letter9").html(letterdice3[ldice][0][0]);
 	$("#letter9").css('color', 'white');
 	$("#letter9").css('background-color', backgroundcolor(letterdice3[ldice][0][1]));
+	$("#letter9").on('click', function() {
+		$("#letter9").addClass('box-shadow');
+	});
 	$("#letter10").html(letterdice3[ldice][1][0]);
 	$("#letter10").css('color', 'white');
 	$("#letter10").css('background-color', backgroundcolor(letterdice3[ldice][1][1]));
+	$("#letter10").on('click', function() {
+		$("#letter10").addClass('box-shadow');
+	});
 	$("#letter11").html(letterdice3[ldice][2][0]);
 	$("#letter11").css('color', 'white');
 	$("#letter11").css('background-color', backgroundcolor(letterdice3[ldice][2][1]));
+	$("#letter11").on('click', function() {
+		$("#letter11").addClass('box-shadow');
+	});
 	$("#letter12").html(letterdice3[ldice][3][0]);
 	$("#letter12").css('color', 'white');
 	$("#letter12").css('background-color', backgroundcolor(letterdice3[ldice][3][1]));
+	$("#letter12").on('click', function() {
+		$("#letter12").addClass('box-shadow');
+	});
 }
 
